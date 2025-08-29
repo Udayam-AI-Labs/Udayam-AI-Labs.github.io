2c26c31a81188db5bf23acc61792115eeacfc491
VishalDesai <desaidesai62230@gmail.com>
2025-08-29 23:26:37 +0530

    Refactor: Redesign the hero-section

--- js/script.js
+++ js/script.js
@@ -291,3 +291,13 @@ function showAlert(event) {
     
     return false;
 }
+
+const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    menuToggle.innerHTML = navLinks.classList.contains("active") 
+        ? '<i class="fa-solid fa-xmark"></i>' 
+        : '<i class="fa-solid fa-bars"></i>';
+});
